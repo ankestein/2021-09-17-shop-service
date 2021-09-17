19cedfcc72bda23b95aa1f78120502a42fa2afb6
Anke <a.stein@posteo.de>
2021-09-17 11:21:10 +0200

    Create Order and OrderRepo

--- src/main/java/de/neuefische/model/Order.java
+++ src/main/java/de/neuefische/model/Order.java
@@ -1,4 +1,35 @@
 package de.neuefische.model;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Order {
+        private int id;
+        private List<Product> products;
+
+        public Order(int id, List<Product> products){
+                this.id = id;
+                this.products = products;
+        }
+
+        public int getId(){
+                return id;
+        }
+
+       public List<Product> getProducts(){
+                return products;
+       }
+
+        @Override
+        public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                Order order = (Order) o;
+                return id == order.id && Objects.equals(products, order.products);
+        }
+
+        @Override
+        public int hashCode() {
+                return Objects.hash(id, products);
+        }
 }
--- src/main/java/de/neuefische/repo/OrderRepo.java
+++ src/main/java/de/neuefische/repo/OrderRepo.java
@@ -1,4 +1,41 @@
 package de.neuefische.repo;
 
+import de.neuefische.model.Order;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class OrderRepo {
+    private Map<Integer, Order> orders = new HashMap<>();
+
+    public List<Order> list(){
+        return orders.values();
+    }
+
+    public Order get(int id) {
+        return Order.getId(id);
+    }
+
+    public void add(Order newOrder){
+        orders.put(newOrder.getId(), newOrder);
+    }
+
+    public void add(List<Order> )
+
+    public void add(Student student) {
+        students.put(student.getId(), student);
+    }
+
+    public void add(List<Student> students) {
+        for (Student student : students) {
+            add(student);
+        }
+    }
+
+
+
 }
+
+
