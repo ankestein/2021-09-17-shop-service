03c4bbb437aff9173f87467034de0af7eb1d3289
Max Lenz <maxlenz@mail.de>
2021-09-17 12:00:19 +0200

    add body for methods

--- src/main/java/de/neuefische/service/ShopService.java
+++ src/main/java/de/neuefische/service/ShopService.java
@@ -5,7 +5,9 @@ import de.neuefische.model.Product;
 import de.neuefische.repo.OrderRepo;
 import de.neuefische.repo.ProductRepo;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ShopService {
@@ -18,23 +20,23 @@ public class ShopService {
         this.orderRepo = orderRepo;
     }
 
-    public String getProduct(){
-        return null;
+    public Product getProduct(int id){
+        return productRepo.get(id);
     }
 
-    public void listProduct(){
-
+    public List<Product> listProducts(){
+        return new ArrayList<>(productRepo.values());
     }
 
-    public void addOrder(){
-
+    public void addOrder(int id, Order value){
+        orderRepo.put(id, value);
     }
 
-    public void getOrder(){
-
+    public Order getOrder(int id){
+        return orderRepo.get(id);
     }
 
-    public void listOrders(){
-
+    public List<Order> listOrders(){
+        return new ArrayList<>(orderRepo.values());
     }
 }
