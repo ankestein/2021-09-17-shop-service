abad909d1f8e6d7801372bddea427a624a0142c1
Max Lenz <maxlenz@mail.de>
2021-09-17 10:53:43 +0200

    fix constructor

--- src/main/java/de/neuefische/service/ShopService.java
+++ src/main/java/de/neuefische/service/ShopService.java
@@ -1,16 +1,19 @@
 package de.neuefische.service;
 
+import de.neuefische.model.Order;
 import de.neuefische.model.Product;
 import de.neuefische.repo.OrderRepo;
 import de.neuefische.repo.ProductRepo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ShopService {
 
-    private ProductRepo productRepo;
-    private OrderRepo orderRepo;
+    private Map<Integer, Product> productRepo = new HashMap<>();
+    private Map<Integer, Order> orderRepo = new HashMap<>();
 
-
-    public ShopService(ProductRepo productRepo, OrderRepo orderRepo) {
+    public ShopService(Map<Integer, Product> productRepo, Map<Integer, Order> orderRepo) {
         this.productRepo = productRepo;
         this.orderRepo = orderRepo;
     }
