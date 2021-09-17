b754690e9583e7afb2b7e3b2c715191da9a1cbaf
Max Lenz <maxlenz@mail.de>
2021-09-17 10:39:03 +0200

    initialize methods

--- src/main/java/de/neuefische/service/ShopService.java
+++ src/main/java/de/neuefische/service/ShopService.java
@@ -1,4 +1,37 @@
 package de.neuefische.service;
 
+import de.neuefische.model.Product;
+import de.neuefische.repo.OrderRepo;
+import de.neuefische.repo.ProductRepo;
+
 public class ShopService {
+
+    private ProductRepo productRepo;
+    private OrderRepo orderRepo;
+
+
+    public ShopService(ProductRepo productRepo, OrderRepo orderRepo) {
+        this.productRepo = productRepo;
+        this.orderRepo = orderRepo;
+    }
+
+    public String getProduct(){
+        return null;
+    }
+
+    public void listProduct(){
+
+    }
+
+    public void addOrder(){
+
+    }
+
+    public void getOrder(){
+
+    }
+
+    public void listOrders(){
+
+    }
 }
