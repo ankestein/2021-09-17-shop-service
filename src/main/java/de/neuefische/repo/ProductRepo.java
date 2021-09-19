b67116386171646e9b7a2582a28400c1cc578fe1
Nico Keskin <nico_keskin_91@hotmail.de>
2021-09-19 15:43:59 +0200

    update product and product-repo for dynamic input in main

--- src/main/java/de/neuefische/repo/ProductRepo.java
+++ src/main/java/de/neuefische/repo/ProductRepo.java
@@ -6,26 +6,26 @@ import java.util.*;
 
 public class ProductRepo {
 
-    private static Map<Integer, Product> products;
+    public Map<Integer, Product> products = new HashMap<>();
 
-    static {
-        Product product1 = new Product(1,"Toilet Paper");
-        Product product2 = new Product(2,"Toothbrush");
-        Product product3 = new Product(3,"Detergent");
-        products = new HashMap<Integer, Product>() {
-            {
-                put(product1.getId(), product1);
-                put(product2.getId(), product2);
-                put(product3.getId(), product3);
-            }
-        };
+    public ProductRepo(List<Product> productsInput) {
+        for (Product product : productsInput) {
+            products.put(product.getId(), product);
+        }
     }
 
-    public static List<Product> list() {
+    public List<Product> list() {
         return new ArrayList<Product>(products.values());
     }
 
-    public static Optional<Product> get(int id) {
+    public Optional<Product> get(int id) {
         return Optional.ofNullable(products.get(id));
+    }
+
+    @Override
+    public String toString() {
+        return "ProductRepo{" +
+                "products=" + products +
+                '}';
     }
 }
